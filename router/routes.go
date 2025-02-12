bb47a5771be1df7b31fe8155be95211c6570cd6a
LeviMaycon <levioficial361@gmail.com>
2025-02-12 16:57:00 -0300

    refactor: remove unnecessary comments

--- router/routes.go
+++ router/routes.go
@@ -9,35 +9,30 @@ import (
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		// GET opening
 		v1.GET("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "GET Opening",
 			})
 		})
 
-		// POST opening
 		v1.POST("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "POST Opening",
 			})
 		})
 
-		// DELETE opening
 		v1.DELETE("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "DELETE Opening",
 			})
 		})
 
-		// PATCH opening
 		v1.PATCH("/opening/:id", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "PATCH Opening",
 			})
 		})
 
-		// GET opening by ID
 		v1.GET("/opening/:id", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "GET Opening by ID",
