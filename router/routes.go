2cbadbac42035b48ca56ade06b105673a6b9b887
LeviMaycon <levioficial361@gmail.com>
2025-02-12 17:20:16 -0300

    refactor: move route handlers to separate handler package

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,17 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/LeviMaycon/oportunities/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-
-		v1.PATCH("/opening/:id", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PATCH Opening",
-			})
-		})
-
-		v1.GET("/opening/:id", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening by ID",
-				"id":  ctx.Param("id"),
-			})
-		})
+		v1.GET("/opening", handler.ListOpeningsHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening/:id", handler.UpdateOpeningHandler)
+		v1.GET("/opening/:id", handler.ListOpeningsByIdHandler)
 	}
 }
